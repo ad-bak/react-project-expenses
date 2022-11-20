1dd60cde6308004df2c48d5fd60987ba711000ad
Adnan <adbak.dev@gmail.com>
2022-11-20 01:36:35 +0100

    added ExpenseForm-s

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,34 @@
+import React, { useState } from "react";
+
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      {props.expenses.map((el) => (
-        <ExpenseItem
-          key={el.id}
-          title={el.title}
-          amount={el.amount}
-          date={el.date}
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
         />
-      ))}
-    </Card>
+        {props.expenses.map((el) => (
+          <ExpenseItem
+            key={el.id}
+            title={el.title}
+            amount={el.amount}
+            date={el.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
 };
 export default Expenses;
